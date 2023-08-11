8f12ac942634ee5d5ed74a0f63d361820a0ccf09
Rafi Matchen <rafimatchen@gmail.com>
2023-08-11 15:46:45 -0400

    updates

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
+"""
+HTMX Test
+"""
+
+import json
+from pathlib import Path
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
+    """
+    index
+    """
     return render_template("index.html")
 
-@app.route("/form")
-def form():
-    return render_template("form.html")
+@app.route("/artists")
+def artists():
+    """
+    Billboard 100 artists
+    """
+    data_path = Path(r"data/artists.json")
+    with open(data_path, encoding="utf-8") as file:
+        data = json.load(file)
+    return render_template("artists", data=data["artists"])
