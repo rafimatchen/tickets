b5f51d440fd4688c9b15ab9b53155a3b8ba4b88e
Rafi Matchen <rafimatchen@gmail.com>
2023-08-12 14:39:07 -0400

    working toggle button

--- app.py
+++ app.py
@@ -10,18 +10,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def index() ->str:
     """
     index
     """
     return render_template("index.html")
 
 @app.route("/artists")
-def artists():
+def artists() -> str:
     """
     Billboard 100 artists
     """
     data_path = Path(r"data/artists.json")
     with open(data_path, encoding="utf-8") as file:
         data = json.load(file)
-    return render_template("artists", data=data["artists"])
+    return render_template("artists.html", data=data["artists"])
+
+@app.route("/main")
+def main() -> str:
+    """
+    main
+    """
+    return render_template("main.html")
